e333a750b96b5b1fb19b6658271fe961d4c0f457
flaviobb <flaviobb@stm.jus.br>
2018-09-21 20:52:56 -0300

    Removendo código para interromper que estava atrapalhando mais que ajudando

---
+++ server_install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install aptitude git ghc cabal-install libghc-text-dev
+git clone https://github.com/nuncanada/OneWayFrameFingerprint.git
+cd OneWayFrameFingerprint/
+cabal update
+cabal install
--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,7 @@ module Main (main) where
 import Client (client)
 import Server (server)
 
-import System.IO
-import System.Exit
-import Data.Char (toLower)
-
 import Control.Concurrent
-import Control.Monad
 
 cloudServers :: [String]
 cloudServers = ["losangeles", "quebec", "saopaulo", "thedalles", "ashburn"]
@@ -18,13 +13,5 @@ udpPort = "1905"
 
 main :: IO ()
 main = do
-    _ <- forkIO $ server udpPort
     _ <- forkIO $ client cloudServers udpPort
-    exitOnQ
-
-exitOnQ :: IO ()
-exitOnQ = do
-    hSetBuffering stdin NoBuffering
-    c <- getChar
-    when (toLower c /= 'q') exitOnQ
-    exitSuccess  -- or "exitWith" and some ExitCode value, use hoogle.
+    server udpPort
