082bc7bbf0a5bf80dc68f7791e45e84db365206d
Flávio Botelho <fezsentido@gmail.com>
2020-11-03 10:27:35 -0500

    Novo servers

--- haskell-daemon-udp/server_install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get -y install aptitude git ghc cabal-install libghc-text-dev
-git clone https://github.com/nuncanada/OneWayFrameFingerprint.git
-cd OneWayFrameFingerprint/
-cabal update
-cabal install
